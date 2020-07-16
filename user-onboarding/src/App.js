7cb506af0afbad9bd3ffc7a894af90354a88ff38
furno west <furnowest57@gmail.com>
2020-07-16 14:23:40 -0700

    done with for form.js

---
+++ user-onboarding/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Form from "./component/Form"
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome New User</h1>
+      <Form/>
+    </div>
+  );
+}
+
+export default App;
